ae1fe639e528c71371e6f63fe445c5daadcf906a
Karthik Viswanathan <karthik.ksv@gmail.com>
2017-03-01 16:26:12 -0800

    Cache dep registry.

--- dep-registry.js
+++ dep-registry.js
@@ -2,39 +2,43 @@ const fs = require("fs");
 const path = require("path");
 const repl = require("repl");
 const vm = require("vm");
+const os = require("os");
+const crypto = require("crypto");
 
 const parser = require("./parser");
 const { promisify } = require("./common");
 const readFile = promisify(fs.readFile);
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
+const writeFile = promisify(fs.writeFile);
 
 const identRegex = /^[$a-z_][0-9a-z_$]*$/i;
 const propsScript = new vm.Script("props = Object.keys(require(id));");
-
-const priorities = {
-  file: 1,
-  pkg: 2,
-  node: 3,
-  inferred: 4
-};
 
 module.exports = class DepRegistry {
   /* public interface */
 
   static populate() {
+    if (this.promise) {
+      return this.promise;
+    }
     if (this.instance) {
       return Promise.resolve(this.instance);
     }
 
     const instance = new DepRegistry();
-    return instance.populate().then(() => {
+    this.promise = instance.populate().then(() => {
       this.instance = instance;
       return instance;
     });
+    return this.promise;
   }
 
   constructor() {
+    // registry[id] is a dep that has the given id
+    this.registry = {};
+
+    // deps[name] is a dep that corresponds to the identifier name
     this.deps = {};
   }
 
@@ -45,24 +49,37 @@ module.exports = class DepRegistry {
   /* private interface */
 
   populate() {
-    // TODO: should we get props for standard libraries?
-    repl._builtinLibs.forEach(id => {
-      this.populateIdents(id, priorities.node);
-      this.populateProps(id, priorities.node);
-    });
-
-    return this.findPkgMeta().then(([meta, root]) => {
-      if (meta) {
-        const ids = Object.keys(meta.dependencies)
-          .concat(Object.keys(meta.devDependencies));
-        ids.forEach(id => {
-          this.populateIdents(id, priorities.pkg);
-          this.populateProps(id, priorities.pkg, root);
+    return this
+      .findPkgMeta()
+      .then(() => this.readCache())
+      .then(cache => {
+        // TODO: should we get props for standard libraries?
+        repl._builtinLibs.forEach(id => {
+          const entry = this.register(cache, id, process.version);
+          if (entry) {
+            this.populateIdents(entry, id);
+            this.populateProps(entry, id);
+          }
         });
-      }
 
-      return this.populateDir(root);
-    });
+        const dependencies = Object.assign(
+          {},
+          this.meta.devDependencies,
+          this.meta.dependencies
+        );
+
+        for (const id in dependencies) {
+          const entry = this.register(cache, id, dependencies[id]);
+          if (entry) {
+            this.populateIdents(entry, id);
+            this.populateProps(entry, path.join(this.dir, "node_modules", id));
+          }
+        }
+
+        return this.populateDir(cache, this.dir);
+      })
+      .then(() => writeFile(this.cachePath, JSON.stringify(this.registry)))
+      .then(() => this.computeDeps());
   }
 
   findPkgMeta(dir) {
@@ -81,10 +98,39 @@ module.exports = class DepRegistry {
           throw err;
         }
       })
-      .then(json => [JSON.parse(json), dir]);
+      .then(json => {
+        this.meta = JSON.parse(json);
+        this.dir = dir;
+
+        const hash = crypto.createHash("sha256");
+        hash.update(dir);
+        this.cachePath = path.join(os.tmpdir(), hash.digest("hex"));
+      });
   }
 
-  populateIdents(id, priority) {
+  readCache() {
+    return readFile(this.cachePath, "utf8")
+      .catch(err => {
+        if (err.code !== "ENOENT") {
+          throw err;
+        }
+        return {};
+      })
+      .then(contents => {
+        try {
+          return JSON.parse(contents);
+        } catch (err) {
+          if (
+            err.name !== "SyntaxError" || err.message.indexOf("JSON") === -1
+          ) {
+            throw err;
+          }
+          return {};
+        }
+      });
+  }
+
+  populateIdents(entry, id) {
     let base;
     if (id.indexOf("/") === -1) {
       base = id;
@@ -93,7 +139,7 @@ module.exports = class DepRegistry {
     }
 
     if (identRegex.test(base)) {
-      this.register({ id, priority, name: base, isProp: false });
+      entry.idents.push(base);
     }
 
     const camelCase = base
@@ -103,27 +149,25 @@ module.exports = class DepRegistry {
       .join("");
 
     if (camelCase.length > 0) {
+      if (camelCase !== base) {
+        entry.idents.push(camelCase);
+      }
+
       const classCase = camelCase[0].toUpperCase() + camelCase.slice(1);
-      // TODO: handle case of multiple matches for same ident?
-      this.register({ id, priority, name: camelCase, isProp: false });
-      this.register({ id, priority, name: classCase, isProp: false });
+      entry.idents.push(classCase);
     }
   }
 
-  populateProps(id, priority, root = null) {
+  populateProps(entry, id) {
     // extract props of this dep by running Object.keys() in a V8 VM
-    const context = {
-      require,
-      id: root ? path.join(root, "node_modules", id) : id
-    };
+    const context = { require, id };
     propsScript.runInNewContext(context);
     if (context.props) {
-      context.props.forEach(name =>
-        this.register({ name, id, priority, isProp: true }));
+      context.props.forEach(p => entry.props.push(p));
     }
   }
 
-  populateDir(dir) {
+  populateDir(cache, dir) {
     return readdir(dir).then(files => {
       const promises = files.map(file => {
         if (file[0] === ".") {
@@ -133,14 +177,18 @@ module.exports = class DepRegistry {
         const filePath = path.join(dir, file);
         return stat(filePath).then(s => {
           const ext = path.extname(file);
+
           if (s.isFile() && (ext === ".js" || ext === ".jsx")) {
-            return this.populateFile(filePath);
+            const entry = this.register(cache, filePath, s.mtime.getTime());
+            if (entry) {
+              return this.populateFile(entry, filePath);
+            }
           } else if (
             s.isDirectory() &&
             file !== "node_modules" &&
             file !== "bower_components"
           ) {
-            return this.populateDir(filePath);
+            return this.populateDir(cache, filePath);
           }
         });
       });
@@ -149,28 +197,60 @@ module.exports = class DepRegistry {
     });
   }
 
-  populateFile(filePath) {
+  populateFile(entry, filePath) {
     return readFile(filePath, "utf8").then(code => {
       const exports = parser.run(code);
       if (exports.hasExports) {
-        exports.idents.forEach(name => this.register({
-          name,
-          id: filePath,
-          priority: priorities.file,
-          isProp: false
-        }));
-        exports.props.forEach(name => this.register({
-          name,
-          id: filePath,
-          priority: priorities.file,
-          isProp: true
-        }));
-        this.populateIdents(filePath, priorities.file);
+        exports.idents.forEach(i => entry.idents.push(i));
+        exports.props.forEach(p => entry.props.push(p));
+        this.populateIdents(entry, filePath);
       }
     });
   }
 
-  register({ name, id, priority, isProp }) {
+  register(cache, id, version) {
+    if (!cache[id] || cache[id].version !== version) {
+      // TODO: notify user?
+      const entry = {
+        version,
+        idents: [],
+        props: []
+      };
+      this.registry[id] = entry;
+      return entry;
+    } else {
+      this.registry[id] = cache[id];
+      // don't return entry; the cached data is valid
+      return null;
+    }
+  }
+
+  computeDeps() {
+    const node = {};
+    repl._builtinLibs.forEach(id => node[id] = true);
+
+    for (const id in this.registry) {
+      let priority;
+      if (node[id]) {
+        // node library; lowest priority
+        priority = 3;
+      } else if (id.indexOf("/") === -1) {
+        // project file; highest priority
+        priority = 1;
+      } else {
+        // package.json dependency; middle priority
+        priority = 2;
+      }
+
+      const { idents, props } = this.registry[id];
+      idents.forEach(name =>
+        this.associate({ name, id, priority, isProp: false }));
+      props.forEach(name =>
+        this.associate({ name, id, priority, isProp: true }));
+    }
+  }
+
+  associate({ name, id, priority, isProp }) {
     const dep = this.deps[name];
     if (
       !dep ||
